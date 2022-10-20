78786ea39e5d2341d678dba5ea74d9c46d18fc7a
takiyac1 <112056951+takiyac1@users.noreply.github.com>
2022-10-20 15:30:34 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿string[] studentNames = new string[] { "Bobby", "Crystal", "Johnny"};
+
+string[] favFood = new string[] {"Pizza", "Salad", "Soup"};
+
+string[] homeTown = new string[] {"Phoenix", "Detroit", "Charlotte"};
+
+
+
+for (int i = 0; i < studentNames.Length; i++)
+{
+    Console.WriteLine("Please select a student by number.. Options are 1-3.");
+    int name = int.Parse(Console.ReadLine());
+    if (name < 1 || name > studentNames.Length)
+    { Console.WriteLine("Invalid number, try again."); }
+    Console.WriteLine($"{studentNames[name - 1]}");
+
+    Console.WriteLine($"What do you want to know about {studentNames[name - 1]}? hometown or favoritefood?");
+    string info = Console.ReadLine();
+    if (info != "hometown" || info != "favoritefood")
+    {
+        Console.WriteLine("Invalid input.");
+        Console.WriteLine($"What do you want to know about {studentNames[name - 1]}? hometown or favoritefood?");
+       
+    }
+
+  string infos = Console.ReadLine();
+    Console.WriteLine($"{infos[name -1]}");
+
+    Console.WriteLine("Would you like to learn about another student? (y/n)");
+    string end = Console.ReadLine();
+
+   if (end == "y")
+    { continue; }
+     else
+    { return; }
+}
